bf64c635697fc1a2f62b5b9874b4297c180b3e8b
Hicham Moustaid <hmoustaid.lrn@gmail.com>
2023-06-09 09:18:20 +0200

    Add styling and content for error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,7 +1,22 @@
-function Error(){
-    return(
-        <h1>Error</h1>
-    )
+import { Link } from 'react-router-dom'
+
+import './index.css'
+
+function Error({
+	errorCode = '404',
+	message = "Oups! La page que vous demandez n'existe pas.",
+}) {
+	return (
+		<section className="error">
+			<h1 className="error-code">{errorCode}</h1>
+			<h2 className="error-message">
+				{message}
+			</h2>
+			<Link className="error-link" to="/">
+				Retourner sur la page d'accueil
+			</Link>
+		</section>
+	)
 }
 
 export default Error
