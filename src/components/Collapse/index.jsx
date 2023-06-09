cbce7c3cba8a132b074d2a662c641d2944c6276e
Hicham Moustaid <hmoustaid.lrn@gmail.com>
2023-06-09 12:50:24 +0200

    Add toggle functionality to Collapse component + About page finished

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -5,13 +5,30 @@ import chevron from '../../assets/chevron.svg'
 import './index.css'
 
 function Collapse({ title, content }) {
+    const [active, setActive] = useState('')
+    const [height, setHeight] = useState('0px')
+    const [rotate, setRotate] = useState('collapse-icon')
+
+    const contentCollapse = useRef(null)
+
+    const toggleCollapse = () => {
+        setActive(active === '' ? 'active' : '')
+        setRotate(
+			active ? 'collapse-icon' : 'collapse-icon rotate'
+		)
+		setHeight(
+			active ? '0px' : `${contentCollapse.current.scrollHeight}px`
+		)
+
+	}
+
 	return (
 		<div className="collapse-section">
-			<button className="collapse">
+			<button className={`collapse ${active}`} onClick={toggleCollapse}>
 				<span className="collapse-title">{title}</span>
-				<img src={chevron} alt="chevron" className= "collapse-icon"/>
+				<img src={chevron} alt="chevron" className= {`${rotate}`}/>
 			</button>
-			<div className="collapse-content">
+			<div ref={contentCollapse} className="collapse-content" style={{ maxHeight: `${height}` }}>
 				<div className="collapse-text">
 						<div>{content}</div>
 				</div>
