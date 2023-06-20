59216e77978ef2d21e4c61a46e1f0d93f1f1d696
Hicham Moustaid <hmoustaid.lrn@gmail.com>
2023-06-20 02:04:27 +0200

    Fixing the display of Équipements

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -21,6 +21,17 @@ function Collapse({ title, content }) {
 		)
 
 	}
+	/*Collapse est utilisé par 'À propos' et par 'Équipements' avec l'objet 'content' comme string dans le primer cas et array dans le 
+	* second, donc on a besoin de représenter cet objet de façon unique pour pouvoir le réutiliser.
+	*/
+	const contentArray = []
+	if (!Array.isArray(content)) {
+		contentArray.push(content)
+	} else {
+		for (let i = 0; i < 9; i++) {
+			contentArray.push(content[i])
+		}
+	}
 
 	return (
 		<div className="collapse-section">
@@ -30,7 +41,9 @@ function Collapse({ title, content }) {
 			</button>
 			<div ref={contentCollapse} className="collapse-content" style={{ maxHeight: `${height}` }}>
 				<div className="collapse-text">
+					{contentArray.map(content => (
 						<div>{content}</div>
+					))}
 				</div>
 			</div>
 		</div>
